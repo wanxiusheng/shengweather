1f65fd760d874217099aadb0ca7903580b2934dc
wan <1066551049@qq.com>
2017-03-07 11:13:14 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/shengweather/android/db/Province.java
@@ -0,0 +1,40 @@
+package com.shengweather.android.db;
+
+/**
+ * Created by wan on 2017/3/7.
+ */
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
