2e5cb1263d5953ad8d9206a6fc5208990cc861bd
wan <1066551049@qq.com>
2017-03-07 19:26:39 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/shengweather/android/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.shengweather.android.util;
+
+/**
+ * Created by wan on 2017/3/7.
+ */
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
